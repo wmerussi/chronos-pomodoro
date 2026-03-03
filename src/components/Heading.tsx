92f4d71c7d4b1846c66ede20631d0daee3b589b3
William Merussi <wmerussi@gmail.com>
2026-03-03 17:36:04 -0300

    adding Heading component

---
+++ src/components/Heading.tsx
@@ -0,0 +1,10 @@
+import type React from 'react';
+import styles from './Heading.module.css';
+
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>;
+}
